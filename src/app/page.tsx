6bfef88badf26a635383f38bb7a54d77f91b357e
sovifi <chris.garcia87@gmail.com>
2024-11-12 15:17:30 -0600

    Initial commit: Sovifi landing page

---
+++ src/app/page.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import Image from "next/image";
+import { ConnectButton } from "thirdweb/react";
+import sovifiIcon from "@public/sovifi.svg";
+import { client } from "./client";
+
+export default function Home() {
+  return (
+    <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
+      <div className="py-20">
+        <Header />
+
+        <div className="flex justify-center mb-20">
+          <ConnectButton
+            client={client}
+            appMetadata={{
+              name: "Example App",
+              url: "https://example.com",
+            }}
+          />
+        </div>
+
+        <ThirdwebResources />
+      </div>
+    </main>
+  );
+}
+
+function Header() {
+  return (
+    <header className="flex flex-col items-center mb-20 md:mb-20">
+      <Image
+        src={sovifiIcon}
+        alt="Sovifi"
+        className="w-[105px] md:w-[105px] mb-12"
+        width={105}
+        height="auto"
+        style={{
+          filter: "drop-shadow(0px 0px 24px rgba(255, 255, 255, 0.65))",
+          objectFit: "contain",
+        }}
+      />
+
+      <h1 className="text-6xl font-bold text-center">
+        sovifi.io
+      </h1>
+
+      <p className="text-zinc-300 text-base">
+        The "Record Label" Ecosystem, Powered By Fans 
+      </p>
+    </header>
+  );
+}
+
+function ThirdwebResources() {
+  return (
+    <div className="grid gap-4 lg:grid-cols-3 justify-center">
+      <ArticleCard
+        title="thirdweb SDK Docs"
+        href="https://portal.thirdweb.com/typescript/v5"
+        description="thirdweb TypeScript SDK documentation"
+      />
+
+      <ArticleCard
+        title="Components and Hooks"
+        href="https://portal.thirdweb.com/typescript/v5/react"
+        description="Learn about the thirdweb React components and hooks in thirdweb SDK"
+      />
+
+      <ArticleCard
+        title="thirdweb Dashboard"
+        href="https://thirdweb.com/dashboard"
+        description="Deploy, configure, and manage your smart contracts from the dashboard."
+      />
+    </div>
+  );
+}
+
+function ArticleCard(props: {
+  title: string;
+  href: string;
+  description: string;
+}) {
+  return (
+    <a
+      href={props.href + "?utm_source=next-template"}
+      target="_blank"
+      className="flex flex-col border border-zinc-800 p-4 rounded-lg hover:bg-zinc-900 transition-colors hover:border-zinc-700"
+    >
+      <article>
+        <h2 className="text-lg font-semibold mb-2">{props.title}</h2>
+        <p className="text-sm text-zinc-400">{props.description}</p>
+      </article>
+    </a>
+  );
+}
